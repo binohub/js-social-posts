acb268175653dcfc020031148f19ddec571089ff
binohub <yeebino001@gmail.com>
2022-07-01 12:13:40 +0200

    ms1 - dynamic img - added

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -97,15 +97,40 @@ const posts = [
     }
 ];
 
-
-const parent = document.querySelector(".likes js-likes");
-posts.forEach(element => {
-    console.log(element);
-
-    const btnLike = document.querySelector(".like-button");
-    btnLike.addEventListener("click", function () {
-    btnLike.classList.add("like-button--liked");
-    // element["likes"] = element["likes"] + 1;
-    // console.log(element["likes"]);
-    })
+parent = document.getElementById("container");
+posts.forEach(post => {
+    console.log(post);
+    const containerPost = document.createElement("div");
+    containerPost.classList.add("post");
+    containerPost.innerHTML = `
+    <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author"></div>
+                        <div class="post-meta__time">4 mesi fa</div>
+                    </div>
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src=${post["media"]} alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    </div>
+                </div>
+            </div>
+    `;
+    parent.append(containerPost);
 });
